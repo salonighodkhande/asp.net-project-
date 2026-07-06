3bb411ef3df8f9017a967cef204933e79bd6842d
salonighodkhande <salonighodkhande@gmail.com>
2026-07-06 22:48:52 +0530

    Add files via upload

---
+++ practical_dotnet/FirstConsoleApp/First.cs
@@ -0,0 +1,10 @@
+using System;
+
+  class First
+  {
+      public void display()
+      {
+          Console.WriteLine("I'm new file named First");
+
+      }
+  }
---
+++ practical_dotnet/FirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+class Program{
+    static void Main(string[] args)
+    {
+            Console.WriteLine("Hello,My name is Saloni");
+            Console.WriteLine("My branch is computer science and engineering.");
+            Console.WriteLine("And my college name is Shri Sant Gajanan Maharaj College of Engineering, Shegaon.");
+
+          First f = new First();
+            f.display();
+}
+}
